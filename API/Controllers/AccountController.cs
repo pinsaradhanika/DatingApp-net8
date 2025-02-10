799b82bd369ef021479e0734a6a8bfeefa5bdf98
pinsaradhanika <pinsaradhanika@gmail.com>
2025-02-11 00:50:40 +0530

    section 04  - task 08

---
+++ API/Controllers/AccountController.cs
@@ -0,0 +1,57 @@
+using System;
+using System.Security.Cryptography;
+using System.Text;
+using API.Data;
+using API.DTOs;
+using API.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Controllers;
+
+public class AccountController(DataContext context) : BaseApiController
+{
+    [HttpPost("register")] //account/register
+
+    public async Task<ActionResult<AppUser>> Register(RegisterDto registerDto)
+    {
+
+        if (await UserExists(registerDto.Username)) return BadRequest("Username is taken");
+
+        using var hmac = new HMACSHA512();
+
+        var user = new AppUser
+        {
+            UserName = registerDto.Username.ToLower(),
+            PasswordHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(registerDto.Password)),
+            PasswordSalt = hmac.Key
+        };
+
+        context.User.Add(user);
+        await context.SaveChangesAsync();
+        
+        return user;
+    }
+
+    [HttpPost("Login")]
+
+    public async Task<ActionResult<AppUser>> Login(LoginDto  loginDto){
+        var user = await context.User.FirstOrDefaultAsync(x => 
+        x.UserName == loginDto.Username.ToLower());
+
+        if(user == null) return Unauthorized("Invalid username");
+
+        using var hmac = new HMACSHA512(user.PasswordSalt);
+        var computedHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(loginDto.Password));
+        
+        for (int i = 0; i < computedHash.Length; i++)
+        {
+            if( computedHash[i] != user.PasswordHash[i]) return Unauthorized("Invalid Password");
+        }
+        return user;
+    }
+
+    public async Task<bool> UserExists(string Username){
+        return await context.User.AnyAsync(x => x.UserName.ToLower() == Username.ToLower()); //Bob != bob
+    }
+}
---
+++ API/DTOs/LoginDto.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API.DTOs
+{
+    public class LoginDto
+    {
+        public required string Username { get; set; }
+
+        public required string Password { get; set; }
+
+    }
+}
