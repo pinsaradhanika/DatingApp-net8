e08cac1c82a26ec6c62cdae3bbaee3cfa1db0ab1
pinsaradhanika <pinsaradhanika@gmail.com>
2025-05-08 11:38:04 +0530

    section 6 topic 2-3 done

--- client/src/app/nav/nav.component.ts
+++ client/src/app/nav/nav.component.ts
@@ -2,23 +2,26 @@ import { Component, inject } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { AccountService } from '../_services/account.service';
 import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
+import { Router, RouterLink, RouterLinkActive } from '@angular/router';
 
 @Component({
   selector: 'app-nav',
   standalone: true,
-  imports: [FormsModule, BsDropdownModule],
+  imports: [FormsModule, BsDropdownModule, RouterLink, RouterLinkActive],
   templateUrl: './nav.component.html',
   styleUrl: './nav.component.css'
 })
 export class NavComponent {
   accountService = inject(AccountService);
+  private router = inject(Router);
   loggedIn = false;
   model: any = {};
 
   login() {
     this.accountService.login(this.model).subscribe({
       next: response => {
-        console.log(response);
+       
+        this.router.navigateByUrl('/members')
       },
       error: error => console.log(error)
     })
@@ -26,6 +29,7 @@ export class NavComponent {
 
   logout(){
     this.accountService.logout();
+    this.router.navigateByUrl('/');
   }
 
 }
