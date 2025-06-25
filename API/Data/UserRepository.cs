0fa6b0eb00b81b921f8f0abe3ba7ec3f345ede6b
pinsaradhanika <pinsaradhanika@gmail.com>
2025-06-25 16:07:53 +0530

    adding photo url to db

--- API/Data/UserRepository.cs
+++ API/Data/UserRepository.cs
@@ -1,12 +1,30 @@
 using System;
+using API.DTOs;
 using API.Entities;
 using API.Interfaces;
+using AutoMapper;
+using AutoMapper.QueryableExtensions;
 using Microsoft.EntityFrameworkCore;
 
 namespace API.Data;
 
-public class UserRepository(DataContext context) : IUserRepository
+public class UserRepository(DataContext context, IMapper mapper) : IUserRepository
 {
+    public async Task<MemberDto?> GetMemberAsync(string username)
+    {
+        return await context.User
+            .Where(x => x.UserName == username)
+            .ProjectTo<MemberDto>(mapper.ConfigurationProvider)
+            .SingleOrDefaultAsync();
+    }
+
+    public async Task<IEnumerable<MemberDto>> GetMembersAsync()
+    {
+        return await context.User
+        .ProjectTo<MemberDto>(mapper.ConfigurationProvider)
+        .ToListAsync();
+    }
+
     public async Task<AppUser?> GetUserByIdAsync(int id)
     {
         return await context.User.FindAsync(id);
@@ -14,12 +32,16 @@ public class UserRepository(DataContext context) : IUserRepository
 
     public async Task<AppUser?> GetUserByUsernameAsync(string username)
     {
-        return await context.User.SingleOrDefaultAsync(x => x.UserName == username);;
+        return await context.User
+        .Include(x => x.Photos)
+        .SingleOrDefaultAsync(x => x.UserName == username);;
     }
 
     public async Task<IEnumerable<AppUser>> GetUsersAsync()
     {
-        return await context.User.ToListAsync();
+        return await context.User
+        .Include(x => x.Photos)
+        .ToListAsync();
     }
 
     public async Task<bool> SaveAllAsync()
